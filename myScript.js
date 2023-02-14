2d1b173afacdb2f882988303561d03d0b1b2d9c3
Rajan Kumar <92258418+rajanchamp98@users.noreply.github.com>
2023-02-14 16:51:22 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -5,14 +5,28 @@ const feedback=document.getElementById("feedback");
 const gender=document.getElementById("gender");
 const form=document.getElementById("form");
 
+let nameData=[]
+let emailData=[]
+let passwordData=[]
+let feedbackData=[]
+let genderData=[]
+
 const btn=document.getElementById("btn");
 
 btn.addEventListener("click",()=>{
-    localStorage.setItem("Name",Name.value)
-    localStorage.setItem("email",email.value)
-    localStorage.setItem("password",password.value)
-    localStorage.setItem("feedback",feedback.value)
-    localStorage.setItem("gender",gender.value)
+    nameData.push(Name.value);
+    emailData.push(email.value);
+    passwordData.push(password.value);
+    feedbackData.push(feedback.value);
+    genderData.push(gender.value);
+
+
+    localStorage.setItem("Name",JSON.stringify(nameData));
+    localStorage.setItem("email",JSON.stringify(emailData));
+    localStorage.setItem("password",JSON.stringify(passwordData));
+    localStorage.setItem("feedback",JSON.stringify(feedbackData));
+    localStorage.setItem("gender",JSON.stringify(genderData));
+  
     alert("Data Saved Sucessfully")
     
 })
